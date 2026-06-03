30759c2b14a9b32189edc12fe7fed7f504acf4c2
VaishnaviKadtan <deepikakadtan15@gmail.com>
2026-06-03 14:54:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -31,43 +31,43 @@ function App(){
     )
 }
 
-class App extends React.Component{
+// class App extends React.Component{
 
-  constructor(props){
-    super(props)
-    this.state = {
-      markdown: "",
-      loading: true
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.changeLoadingVar = this.changeLoadingVar.bind(this);
-  }
-  handleChange(e){
-    this.setState({
-      markdown: e.target.value
-    })
-  }
-  changeLoadingVar(){
-    this.setState({
-      loading: false
-    })
-  }
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       markdown: "",
+//       loading: true
+//     }
+//     this.handleChange = this.handleChange.bind(this);
+//     this.changeLoadingVar = this.changeLoadingVar.bind(this);
+//   }
+//   handleChange(e){
+//     this.setState({
+//       markdown: e.target.value
+//     })
+//   }
+//   changeLoadingVar(){
+//     this.setState({
+//       loading: false
+//     })
+//   }
 
-  componentDidMount(){
-    setTimeout(this.changeLoadingVar,3000)
-  }
-  render(){
-    if(this.state.loading){
-      return (<div><h1>Loading...</h1></div>)
-    }
+//   componentDidMount(){
+//     setTimeout(this.changeLoadingVar,3000)
+//   }
+//   render(){
+//     if(this.state.loading){
+//       return (<div><h1>Loading...</h1></div>)
+//     }
  
-    return(
-      <div className="app">
-        <textarea onChange={this.handleChange}/>
-        <ReactMarkdown className="preview" children={this.state.markdown}/>
-      </div>
-    )
-  }
-}
+//     return(
+//       <div className="app">
+//         <textarea onChange={this.handleChange}/>
+//         <ReactMarkdown className="preview" children={this.state.markdown}/>
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
